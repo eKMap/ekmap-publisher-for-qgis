52875c0147ee47878ea06f8419993a8b8b62288a
Dev 2 <dev2.ekgis@gmail.com>
2021-05-14 14:01:31 +0700

    add line pattern fill

---
+++ ekmap_server/qgslayer_parser/fill_symbol/line_fill_parser.py
@@ -0,0 +1,48 @@
+from PyQt5.QtCore import QSize
+from .fill_layer_parser import FillLayerParser
+from ...ekmap_converter import eKConverter
+from ...ekmap_common import *
+
+import hashlib, json, importlib.util
+from pathlib import Path 
+
+class LineFillParser(FillLayerParser):
+
+    def __init__(self, lineFillLayer, exporter):
+        super().__init__(lineFillLayer)
+
+        spacing = lineFillLayer.distance()
+        spacingUnit = eKConverter.convertRenderUnitValueToName(lineFillLayer.distanceUnit())
+        spacing = int(eKConverter.convertUnitToPixel(spacing, spacingUnit))
+
+        rotation = int(lineFillLayer.lineAngle())
+
+        file = Path(__file__).resolve()
+        parent = file.parent.parent
+        spec = importlib.util.spec_from_file_location("eKMap-Publisher-For-QGIS.ekmap_server.qgslayer_parser.symbol_layer_factory", 
+            str(parent) + "/symbol_layer_factory.py")
+        foo = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(foo)
+        subSymbolStyle = []
+        for subSymbol in lineFillLayer.subSymbol():
+            borderStyle = foo.SymbolLayerFactory \
+                .getLayerParser(subSymbol, exporter)
+            subSymbolStyle.extend(borderStyle.parse())
+        
+        linePatternName = json.dumps(subSymbolStyle)
+        linePatternName = hashlib.md5(linePatternName.encode()).hexdigest()
+        
+        if rotation > 0:
+            linePatternName = linePatternName \
+                + '_R' + str(rotation)
+
+        dstPath = TEMP_LOCATION + '/' + linePatternName + '.png'
+        lineFillLayer.subSymbol().exportImage(dstPath, 'png', QSize(spacing, spacing))
+        exporter.externalGraphics.append(dstPath)
+
+        fillConfig = {
+            'fill-pattern': linePatternName,
+            'fill-opacity': 1
+        }
+        fillStyleLayer = self.exportFillLayerFormat(fillConfig)
+        self.styles.append(fillStyleLayer)
--- ekmap_server/qgslayer_parser/symbol_layer_factory.py
+++ ekmap_server/qgslayer_parser/symbol_layer_factory.py
@@ -7,11 +7,25 @@ from .fill_symbol.simple_fill_parser import SimpleFillParser
 from .fill_symbol.svg_fill_parser import SVGFillParser
 from .fill_symbol.raster_fill_parser import RasterFillParser
 from .fill_symbol.point_fill_parser import PointFillParser
+from .fill_symbol.line_fill_parser import LineFillParser
 
 class SymbolLayerFactory():
 
     def getLayerParser(symbolLayer, exporter):
         layerType = symbolLayer.layerType()
+        # parser = {
+        #     'SimpleMarker': SimpleMarkerParser(symbolLayer),
+        #     'RasterMarker': RasterMarkerParser(symbolLayer),
+        #     'SvgMarker': SvgMarkerParser(symbolLayer),
+        #     'FontMarker': FontMarkerParser(symbolLayer),
+        #     'SimpleLine': SimpleLineParser(symbolLayer),
+        #     'SimpleFill': SimpleFillParser(symbolLayer, exporter),
+        #     'SVGFill': SVGFillParser(symbolLayer, exporter),
+        #     'RasterFill': RasterFillParser(symbolLayer, exporter),
+        #     'PointPatternFill': PointFillParser(symbolLayer, exporter),
+        #     'LinePatternFill': LineFillParser(symbolLayer, exporter)
+        # }
+        # return parser.get(layerType, None)
         if layerType ==  'SimpleMarker':
             return SimpleMarkerParser(symbolLayer)
         elif layerType == 'RasterMarker':
@@ -30,4 +44,6 @@ class SymbolLayerFactory():
             return RasterFillParser(symbolLayer, exporter)
         elif layerType == 'PointPatternFill':
             return PointFillParser(symbolLayer, exporter)
+        elif layerType == 'LinePatternFill':
+            return LineFillParser(symbolLayer, exporter)
         return None
